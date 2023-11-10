b6dd7149a6fc1430a40c529185ba240372f1a467
IlyasIshmullin <ilyas.ishmul@gmail.com>
2023-11-10 14:12:57 +0300

    CORS

--- yats/settings.py
+++ yats/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "contest",
     "rest_framework",
     "rest_framework.authtoken",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = "yats.urls"
